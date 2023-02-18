a4aac57cbdc6704ab9390071269c04ee211fe706
shohana-tisha <tisha311497@gmail.com>
2023-02-19 00:34:12 +0600

    php code

---
+++ php/oddnumbers.php
@@ -0,0 +1,7 @@
+<?php
+for($var =2; $var <=100; $var+=1)
+{
+if($var % 2 ==0)
+echo "$var, ";
+}
+?>
---
+++ php/rectangle.php
@@ -0,0 +1,7 @@
+<?php
+
+ 
+ $length = 14;  
+ $width = 12;  
+ echo "The Area of a rectangle is $length * $width= " . ($length * $width) . "<br />";  
+  ?>  
