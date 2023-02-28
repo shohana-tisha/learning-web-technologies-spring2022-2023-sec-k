ecf1666b5aae6d49bb250e6bc66f9af5a1f8ea2b
shohana-tisha <106137070+shohana-tisha@users.noreply.github.com>
2023-02-28 13:53:29 +0600

    Delete php directory

--- php/oddnumbers.php
+++
@@ -1,7 +0,0 @@
-<?php
-for($var =2; $var <=100; $var+=1)
-{
-if($var % 2 ==0)
-echo "$var, ";
-}
-?>
--- php/rectangle.php
+++
@@ -1,7 +0,0 @@
-<?php
-
- 
- $length = 14;  
- $width = 12;  
- echo "The Area of a rectangle is $length * $width= " . ($length * $width) . "<br />";  
-  ?>  
